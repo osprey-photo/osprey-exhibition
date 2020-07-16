8135a122d57bca2d6509d017efd4551c821db049
MatthewBWhite <matthew@mh-white.com>
2020-07-16 20:34:19 +0100

    Reset code
    Signed-off-by: MatthewBWhite <matthew@mh-white.com>

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Bulma in Vue-CLI
+      </h1>
+
+      <div class="columns is-tablet">
+        <card v-for="i in 4" :key="i" :num="i"></card>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from './Card'
+export default {
+  components: {
+    Card
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
